5399f61a1c52f3c598606812cd13ba28ee97513b
Damien <156458881+damienjianyi@users.noreply.github.com>
2024-05-13 18:06:57 -0600

    Jump Implementation
    he go boing :D

--- GrindingDinos/Assets/Scripts/PlayerController.cs
+++ GrindingDinos/Assets/Scripts/PlayerController.cs
@@ -4,28 +4,58 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour, IButtonListener
 {
+    [SerializeField] Rigidbody2D rigidBody;
+    float jumpHeight = 10.0f;
+    //raycast for grounded
+    [SerializeField] Vector2 boxSize;
+    [SerializeField] float castDistance;
+    [SerializeField] LayerMask groundLayer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        var inputListener = FindObjectOfType<PlayerInputs>();
+        inputListener.RegisterListener(this);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        isGrounded();
     }
     public void ButtonHeld(ButtonInfo heldInfo)
     {
-
+        //grind?
     }
     public void ButtonPressed(ButtonInfo pressedInfo)
     {
-
+        //jump
+        if(isGrounded())
+        {
+            Debug.Log("boing");
+            rigidBody.velocity = new Vector2(0.0f, jumpHeight);
+        }
+        
     }
     public void ButtonReleased(ButtonInfo releasedInfo)
     {
 
     }
-
+    public bool isGrounded()
+    {
+        if (Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, castDistance, groundLayer))
+        {
+            Debug.Log("Grounded");
+            return true;
+        }
+        else
+        {
+            Debug.Log("Not Grounded");
+            return false;
+        }
+            
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position-transform.up * castDistance, boxSize);
+    }
 }
