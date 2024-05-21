75f4e97f38e6d7f0d5d69eb1e9204f588b820619
busch-owen <buschowen@gmail.com>
2024-05-21 11:54:01 -0600

    Started on variable score tick rates

--- GrindingDinos/Assets/Scripts/Utilities/GameManager.cs
+++ GrindingDinos/Assets/Scripts/Utilities/GameManager.cs
@@ -1,9 +1,12 @@
+using UnityEngine.SocialPlatforms.Impl;
+
 public class GameManager : Singleton<GameManager>
 {
     public bool GameStarted { get; private set; }
 
     private SceneController _sceneController;
     private UIManager _uiManager;
+    private ScoreHandler _scoreHandler;
 
     private void Start()
     {
@@ -14,16 +17,19 @@ public class GameManager : Singleton<GameManager>
     {
         GameStarted = true;
         _uiManager.CloseMainMenu();
+        _scoreHandler.StartTickingScoreCounter();
     }
 
     public void DeathSequence()
     {
         _uiManager.OpenDeathMenu();
+        _scoreHandler.StopTickingScoreCounter();
     }
 
     public void RestartGame()
     {
         //All this will do for now is reload the scene, potentially we could add a high score system or something of the sort.
+        _scoreHandler.ResetCurrentScores();
         _sceneController.ReloadCurrentScene();
         Invoke(nameof(ResetGameState), 0.05f);
     }
@@ -33,5 +39,6 @@ public class GameManager : Singleton<GameManager>
         GameStarted = false;
         _sceneController = FindObjectOfType<SceneController>();
         _uiManager = FindObjectOfType<UIManager>();
+        _scoreHandler = FindObjectOfType<ScoreHandler>();
     }
 }
--- GrindingDinos/Assets/Scripts/Utilities/ScoreHandler.cs
+++ GrindingDinos/Assets/Scripts/Utilities/ScoreHandler.cs
@@ -7,35 +7,46 @@ public class ScoreHandler : Singleton<ScoreHandler>
     
     private int _distanceScore;
     private int _trickScore;
+    private int _totalScore;
 
     private int _highScore;
 
     [SerializeField] private int scoreTick;
+    [SerializeField] private int scoreTickRate;
 
     public override void Awake()
     {
         _gameManager = FindObjectOfType<GameManager>();
         _uiManager = FindObjectOfType<UIManager>();
     }
-
-    private void FixedUpdate()
-    {
-        if (!_gameManager.GameStarted)
-            return;
-
-        _distanceScore += scoreTick;
-        _uiManager.UpdateDistanceScore(_distanceScore);
-
-        if (_distanceScore + _trickScore > _highScore)
-        {
-            _highScore = _distanceScore + _trickScore;
-        }
-    }
-
+    
     public void AddToTrickScore(int scoreValue)
     {
         _trickScore += scoreValue;
         _uiManager.UpdateTrickScore(_trickScore);
+    }
+
+    public void AddToDistanceScore()
+    {
+        _distanceScore += scoreTick;
+        _uiManager.UpdateDistanceScore(_distanceScore);
+
+        _totalScore = _distanceScore + _trickScore;
+        if (_totalScore > _highScore)
+        {
+            _highScore = _totalScore;
+        }
+    }
+
+    public void StartTickingScoreCounter()
+    {
+        CancelInvoke(nameof(AddToDistanceScore));
+        InvokeRepeating(nameof(AddToDistanceScore), 0, scoreTickRate);
+    }
+
+    public void StopTickingScoreCounter()
+    {
+        CancelInvoke(nameof(AddToDistanceScore));
     }
 
     public void ResetCurrentScores()
